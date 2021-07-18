8c50e259e12e211d5fba32f21f69a5518f5e3d05
misha <misha-erdyakov@mail.ru>
2021-07-18 19:27:57 +0300

    ...

--- src/com/mihkail/Main.java
+++ src/com/mihkail/Main.java
@@ -1,8 +1,18 @@
+// считывание с консоли в файл
+
 package com.mihkail;
 
+import java.io.*;
+
 public class Main {
-
     public static void main(String[] args) {
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter writer = new BufferedWriter(new FileWriter("res.txt")))
+        {
 
+        }
+        catch(IOException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
---
+++ src/com/mihkail/WalkerException.java
@@ -0,0 +1,7 @@
+package com.mihkail;
+
+public class WalkerException extends Exception {
+    WalkerException(final String message) {
+        super(message);
+    }
+}
